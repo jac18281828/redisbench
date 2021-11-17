b3405c23d1cef71a2776c65a64670171eb3c2a34
John Cairns <john@2ad.com>
2021-11-17 15:44:13 -0600

    slight cleanup

--- pull/pull.py
+++ pull/pull.py
@@ -4,34 +4,38 @@ import json
 import time
 
 SENTINEL = b'########END########'
+TERMINUS = list(SENTINEL)
+
+def pull_redis(h:str):
+    print('Connecting %s' % h)        
+    queue_key = 'Z0' + ':' + 'dispatchqueue'
+    with redis.Redis(host=h, port=6379, db=1) as r:
+        r.lpush(queue_key, b'start')
+        print('Starting')
+        count = 0
+        start = time.time()
+        is_running = True
+        while is_running:
+            txn = r.brpop(queue_key, timeout=1)
+            if txn is not None:
+                payload = txn[1]
+                if len(payload) == len(TERMINUS) and list(payload) == TERMINUS:
+                    is_running = False
+                else:
+                    count += 1
+
+    print('%d events' % count)
+    runtime = time.time() - start
+    print('%f/s' % (count/runtime))
+    sys.stdout.flush()
+    
 
 if __name__ == '__main__':
 
     if len(sys.argv) == 2:
         h = sys.argv[1]
-        print('Connecting %s' % h)        
-
-        queue_key = 'Z0' + ':' + 'dispatchqueue'
-
-        with redis.Redis(host=h, port=6379, db=1) as r:
-            time.sleep(10)
-            r.rpush(queue_key, b'start')
-            print('Starting')
-            count = 0
-            start = time.time()
-            is_running = True
-            while is_running:
-                txn = r.blpop(queue_key, timeout=1)
-                if txn is not None:
-                    payload = txn[1]
-                    if len(txn[1]) == len(SENTINEL) and list(txn[1]) == list(SENTINEL):
-                        is_running = False
-                    else:
-                        count += 1
-
-        print('%d events' % count)
-        runtime = time.time() - start
-        print('%f/s' % (count/runtime))
+        time.sleep(5)
+        pull_redis(h)
     else:
         print('%s hostname' % sys.argv[0])
         sys.exit(1)
--- push/push.py
+++ push/push.py
@@ -4,39 +4,45 @@ import json
 import time
 
 SENTINEL = b'########END########'
+NRUN = 1 # 2000 each
 
+def push_redis(h:str, file:str):
+    queue_key = 'Z0' + ':' + 'dispatchqueue'
+    with redis.Redis(host=h, port=6379, db=1) as r:
+        start_wait = True
+        while start_wait:
+            begin = r.brpop(queue_key, timeout=10)
+            if begin is not None:
+                start_wait = False
+            else:
+                print('Idle')
+        print('Starting')
+        count = 0
+        start = time.time()
+        for _ in range(NRUN):
+            with open(file, 'r') as txnstream:
+                txn = json.load(txnstream)
+                for t in txn:
+                    r.lpush(queue_key, json.dumps(t))
+                    count += 1
+                    len = r.llen(queue_key)
+                    if len > 500:
+                        print('%s len %d' % (queue_key, len))
+                        if len > 10000:
+                            print(' -- Warning Length with Trim! -- ')
+                            r.ltrim(queue_key, 0, 10000)
+        r.lpush(queue_key, SENTINEL)
+        print('%d sent' % count)
+        runtime = time.time() - start
+        print('%f/s' % (count/runtime))
+        sys.stdout.flush()
+        
 if __name__ == '__main__':
 
     if len(sys.argv) == 3:
         h = sys.argv[1]
         file = sys.argv[2]
-
-        print('Connecting %s' % h)
-
-        queue_key = 'Z0' + ':' + 'dispatchqueue'
-
-        with redis.Redis(host=h, port=6379, db=1) as r:
-            start_wait = True
-            while start_wait:
-                begin = r.blpop(queue_key, timeout=10)
-                if begin is not None:
-                    start_wait = False
-                else:
-                    print('Idle')
-            print('Starting')
-            for _ in range(100):
-                with open(file, 'r') as txnstream:
-                    txn = json.load(txnstream)
-                    for t in txn:
-                        r.rpush(queue_key, json.dumps(t))
-                        len = r.llen(queue_key)
-                        if len > 500:
-                            print('%s len %d' % (queue_key, len))
-                            if len > 10000:
-                                print(' -- Warning Length with Trim! -- ')
-                        if len > 10000:
-                            r.ltrim(queue_key, 0, 10000)
-            r.lpush(queue_key, SENTINEL)
+        push_redis(h, file)
     else:
         print('%s hostname filename' % sys.argv[0])
         sys.exit(1)
