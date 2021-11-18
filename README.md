4c25593db87b6c42d932e15af92ef2263f93e151
John Cairns <john@2ad.com>
2021-11-18 12:40:22 -0600

    Include support for scaling apps 1 x N

--- README.md
+++ README.md
@@ -16,7 +16,22 @@ Timing is performed on MacBook Pro running Docker from a Debian stable-slim base
 
 This is a 'best effort' benchmark.   The goal of this work is to produce a ballpark estimate of queue performance for Redis.  Only slight effort has been made to tune the results.
 
+
+### QuickStart 
+
+#### Run with 1 x 1 push - pull
+```
+$ docker-compose build && docker-compose up
+```
+
+### RUn with 1 x 5 push - multiqueue 
+```
+$ docker-compose build && docker-compose up --scale puller=5
+```
+
 ## Results
+
+1x1
 
 ```
 pusher_1  | Starting
@@ -30,4 +45,19 @@ puller_1  | 2192.236769/s
 
 Redis is limiting throughput to around 2000 messages each second
 
+1x5
+```
+pusher_1  | INFO:root:500000 sent on 5 puller
+pusher_1  | INFO:root:2183.174237/s
+puller_1  | INFO:root:100000 events
+puller_1  | INFO:root:435.990702/s
+puller_2  | INFO:root:100000 events
+puller_2  | INFO:root:426.789566/s
+puller_3  | INFO:root:100000 events
+puller_3  | INFO:root:435.790609/s
+puller_4  | INFO:root:100000 events
+puller_4  | INFO:root:435.665158/s
+puller_5  | INFO:root:100000 events
+puller_5  | INFO:root:435.999057/s
+```
 
--- push/push.py
+++ push/push.py
@@ -2,47 +2,103 @@ import redis
 import sys
 import json
 import time
+import logging
 
-SENTINEL = b'########END########'
-NRUN = 250 # 2000 each
+NRUN = 250 # len(txndata.json) each ~ 2000
 
-def push_redis(h:str, file:str):
-    queue_key = 'Z0' + ':' + 'dispatchqueue'
-    with redis.Redis(host=h, port=6379, db=1) as r:
-        start_wait = True
-        while start_wait:
-            begin = r.brpop(queue_key, timeout=10)
-            if begin is not None:
-                start_wait = False
-            else:
-                print('Idle')
-        print('Starting')
-        count = 0
-        start = time.time()
-        for _ in range(NRUN):
+class DataPusher:
+
+    PULLER_KEY = 'puller::count'
+
+    def __init__(self, h:str, file:str):
+        logging.info('Connecting %s' % h)                
+        self.rcli = redis.Redis(host=h, port=6379, db=1)
+        self.file = file
+        self.id = -1
+        
+    def get_ready_key(self, id:int):
+        return 'Z' + str(id) + ':' + 'ready'
+        
+    def get_puller_count(self):
+        n_puller = self.rcli.get(self.PULLER_KEY)
+        if n_puller:
+            return int(n_puller)
+        else:
+            return 0
+
+    def all_puller_ready(self, n_puller:int):
+        for id in range(n_puller):
+            ready_key = self.get_ready_key(id)
+            value = self.rcli.get(ready_key)
+            if value is not None and int(value) == 0:
+                logging.debug('Puller %d not ready' % id)
+                return False
+        return True
+
+    def await_ready(self):
+        last_count = 0
+        while True:
+            n_puller = self.get_puller_count()
+            if n_puller == 0 or n_puller != last_count:
+                last_count = n_puller
+                time.sleep(5)
+                continue
+            elif self.all_puller_ready(n_puller):
+                logging.info('%d puller ready' % n_puller)
+                return
+            time.sleep(0.125)
+
+    def close_pullers(self):
+        n_puller = self.get_puller_count()
+        for id in range(n_puller):
+            ready_key = self.get_ready_key(id)
+            self.rcli.set(ready_key, 0)
+
+    def log_stats(self):
+        logging.info('%d sent on %d puller' % (self.count, self.get_puller_count()))
+        runtime = time.time() - self.start
+        logging.info('%f/s' % (self.count/runtime))
+
+    def push(self):
+        n_puller = self.get_puller_count()
+        logging.info('Starting push on %d apps' % n_puller)
+        self.count = 0
+        self.start = time.time()
+        for _ in range(NRUN):            
             with open(file, 'r') as txnstream:
                 txn = json.load(txnstream)
                 for t in txn:
-                    r.lpush(queue_key, json.dumps(t))
-                    count += 1
-                    len = r.llen(queue_key)
-                    if len > 500:
-                        print('%s len %d' % (queue_key, len))
+                    queue_index = self.count % n_puller
+                    queue_key = 'Z' + str(queue_index) + ':' + 'dispatchqueue'
+                    self.rcli.lpush(queue_key, json.dumps(t))
+                    self.count += 1
+                    len = self.rcli.llen(queue_key)
+                    is_log_once = False
+                    while len > 500:
+                        if not is_log_once:
+                            is_log_once = True
+                            logging.warning('%s len %d' % (queue_key, len))
+                        else:
+                            pass
                         if len > 10000:
-                            print(' -- Warning Length with Trim! -- ')
-                            r.ltrim(queue_key, 0, 10000)
-        r.lpush(queue_key, SENTINEL)
-        print('%d sent' % count)
-        runtime = time.time() - start
-        print('%f/s' % (count/runtime))
-        sys.stdout.flush()
+                            logging.error(' -- Warning Length with Trim! -- ')
+                            self.rcli.ltrim(queue_key, 0, 10000)
+                    if self.count % 25000 == 0:
+                        self.log_stats()
+        self.log_stats()
+
         
 if __name__ == '__main__':
 
     if len(sys.argv) == 3:
         h = sys.argv[1]
         file = sys.argv[2]
-        push_redis(h, file)
+        logging.basicConfig(level=logging.INFO)        
+        pusher = DataPusher(h, file)
+        time.sleep(10)
+        pusher.await_ready()
+        pusher.push()
+        pusher.close_pullers()
     else:
-        print('%s hostname filename' % sys.argv[0])
+        logging.error('%s hostname filename' % sys.argv[0])
         sys.exit(1)
